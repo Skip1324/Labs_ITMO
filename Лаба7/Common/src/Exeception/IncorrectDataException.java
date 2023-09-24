1e3845b2319c33e477bbb7a966886804be03d722
Skip1324 <maks.kazaev05@mail.ru>
2023-09-24 23:52:25 +0300

    AAA
    AAA

--- Лаба7/Common/src/Exeception/IncorrectDataException.java
+++
@@ -1,9 +0,0 @@
-package Exeception;
-
-import java.io.IOException;
-/**
- *Exception witch catch not correct data in collection.
- */
-public class IncorrectDataException extends IOException {
-
-}
--- Лаба7/Common/src/Exeception/QuantityOfArgsException.java
+++
@@ -1,7 +0,0 @@
-package Exeception;
-/**
- *Exception witch catch, when length of args not equals 1.
- */
-public class QuantityOfArgsException extends Exception {
-}
-
